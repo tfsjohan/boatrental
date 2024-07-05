7d14b96ac8f0bc58848d14520e7a6276f62baaeb
Johan Nordberg <johan@ifokus.se>
2024-07-05 11:16:23 +0200

    Added more reasoning.

--- CarRental.PriceService/PriceService.cs
+++ CarRental.PriceService/PriceService.cs
@@ -14,11 +14,16 @@ public class PriceService(ICarTypePriceRepository carTypePriceRepository) : IPri
          * a factory is used to create the correct price calculator based on the car type
          * is likely sufficient for this case.
          *
-         * If there are more car types, and the client should be able to add more car types
+         * If there are more car types, or if the client should be able to add more car types
          * in some admin panel, then a more dynamic solution should be considered.
          * In that case I would probably use a discriminator pattern, where the car type
          * is a string, and the ORM could to create the correct price calculator.
          * Most ORM system support this kind of mapping, so it would be easy to implement.
+         *
+         * I like the use of subclasses, because it makes it really flexible to have
+         * very different types of calculations for different car types. Like,
+         * a regular car is based on days and distance, but a convertable might have
+         * different pricing based on season and a weather forecast.
          */
         var calculator = PriceCalculatorFactory.CreatePriceCalculator(details.CarType);
 
